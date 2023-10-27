49e721c916df31ed6e831c04283b14f5e782786e
suhas pathak <suhas.jio46@gmail.com>
2023-10-27 08:59:01 +0530

    iTour: Created Project

---
+++ iTour/iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Suhas Pathak on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
